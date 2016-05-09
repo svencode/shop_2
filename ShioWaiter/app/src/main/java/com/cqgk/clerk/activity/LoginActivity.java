b916cd44d1c560f78fb3927152e63eba52ec56ff
蒋嘉亮 <33871442@qq.com>
2016-05-09 09:19:30 +0800

    欢迎页登录页提交

--- ShioWaiter/app/src/main/java/com/cqgk/clerk/activity/LoginActivity.java
+++ ShioWaiter/app/src/main/java/com/cqgk/clerk/activity/LoginActivity.java
@@ -1,10 +1,22 @@
 package com.cqgk.clerk.activity;
 
+import android.os.Bundle;
+
+import com.cqgk.clerk.R;
 import com.cqgk.clerk.base.BusinessBaseActivity;
+
+import org.xutils.view.annotation.ContentView;
 
 /**
  * Created by duke on 16/5/6.
  */
+@ContentView(R.layout.login)
 public class LoginActivity extends BusinessBaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableTitleDelegate();
+        getTitleDelegate().setTitle("用户登录");
+    }
 }
--- ShioWaiter/app/src/main/java/com/cqgk/clerk/activity/WelcomeActivity.java
+++ ShioWaiter/app/src/main/java/com/cqgk/clerk/activity/WelcomeActivity.java
@@ -1,10 +1,48 @@
 package com.cqgk.clerk.activity;
 
+import android.os.Bundle;
+import android.os.Message;
+
+import com.cqgk.clerk.R;
 import com.cqgk.clerk.base.BusinessBaseActivity;
+import com.cqgk.clerk.helper.NavigationHelper;
+
+import org.xutils.view.annotation.ContentView;
 
 /**
  * Created by duke on 16/5/6.
  */
+@ContentView(R.layout.welcome)
 public class WelcomeActivity extends BusinessBaseActivity {
 
+    private final int UI_MAIN_TAB = 1;
+    private final int UI_LOGIN = 2;
+    private static final long SPLASH_DELAY_MILLIS = 2000;//延时3秒
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestData();
+    }
+
+
+    @Override
+    public void requestData() {
+        super.requestData();
+        sendHandler(getUIHandler(), UI_MAIN_TAB, SPLASH_DELAY_MILLIS);
+    }
+
+    @Override
+    public void onUIHandleMessage(Message msg) {
+        super.onUIHandleMessage(msg);
+        switch (msg.what) {
+            case UI_MAIN_TAB:
+                NavigationHelper.getInstance().startLoginActivity();
+                finish();
+                break;
+        }
+    }
 }
