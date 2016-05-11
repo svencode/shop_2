47139080606213b85777e49a452083eb97fd7ed3
Sven <234678049@qq.com>
2016-05-11 23:50:18 +0800

    商品筛选

---
+++ ShioWaiter/app/src/main/java/com/cqgk/clerk/activity/PickGoodActivity.java
@@ -0,0 +1,34 @@
+package com.cqgk.clerk.activity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.cqgk.clerk.R;
+import com.cqgk.clerk.adapter.CashieringAdapter;
+import com.cqgk.clerk.base.BusinessBaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+
+/**
+ * Created by sven on 16/5/11.
+ */
+@ContentView(R.layout.activity_cashiering)
+public class PickGoodActivity extends BusinessBaseActivity{
+    @ViewInject(R.id.listView)
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        enableTitleDelegate();
+        getTitleDelegate().setTitle("挑选商品");
+        layoutView();
+    }
+
+
+    private void layoutView(){
+        CashieringAdapter adapter = new CashieringAdapter(this);
+        listView.setAdapter( adapter);
+    }
+}
